c561c311545ce74b81ec980849cd1c94e81d654f
Lebedenko_AE <lebedenko.aleksander2015@yandex.ru>
2026-05-27 15:01:40 +0300

    Добавлена валидация

--- src/main/java/aston/intensiv/notificationservice/dto/EmailRequest.java
+++ src/main/java/aston/intensiv/notificationservice/dto/EmailRequest.java
@@ -1,11 +1,15 @@
 package aston.intensiv.notificationservice.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class EmailRequest {
+    @Email
     private String email;
+    @NotBlank
     private String message;
 }
